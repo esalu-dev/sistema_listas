6114b6ac5fa25abb15940338578130aad285846c
esalu-dev <esalu88@outlook.com>
2023-11-21 08:31:43 -0600

    Fix blurred background and add arrow icon to LoginButton

--- src/LandingPage.jsx
+++ src/LandingPage.jsx
@@ -26,7 +26,7 @@ export function LandingPage() {
   const handleMouseEnter2 = () => {
     setHover2(true);
     setBlurredBackground(
-      "absolute top-0 right-0 w-screen h-screen fondo blur-sm transition-all duration-300"
+      "absolute top-0 right-0 w-screen h-screen fondo blur-[2px] transition-all duration-300"
     );
   };
 
@@ -91,66 +91,4 @@ export function LandingPage() {
       </section>
     </main>
   );
-
-  // return (
-  //   <section className="w-screen h-screen">
-  //     <header className="flex px-10 py-6 justify-between">
-  //       <h1 className="text-xl font-bold">Sistema Integral de Información</h1>
-  //       <nav>
-  //         <ul className="flex gap-10">
-  //           <li>
-  //             <a
-  //               href="https://siit.itdurango.edu.mx/sistema/"
-  //               target="_BLANK"
-  //               rel="noreferrer"
-  //             >
-  //               SII
-  //             </a>
-  //           </li>
-  //           <li>
-  //             <a
-  //               href="https://plataforma.itdurango.edu.mx/"
-  //               target="_BLANK"
-  //               rel="noreferrer"
-  //             >
-  //               Plataforma
-  //             </a>
-  //           </li>
-  //           <li>
-  //             <a href="#">Ayuda</a>
-  //           </li>
-  //         </ul>
-  //       </nav>
-  //     </header>
-  //     <span className="absolute w-screen h-[2px] bg-guinda-900"></span>
-  //     <div className="flex justify-around px-36 py-10 h-[600px] border-2 gap-14 bg-guinda-400">
-  //       <Card align="center" className="flex-1">
-  //         <CardHeader>
-  //           <Heading size="md">Iniciar sesión como estudiante</Heading>
-  //         </CardHeader>
-  //         <CardBody>
-  //           <img src="" alt="" srcSet="" />
-  //         </CardBody>
-  //         <CardFooter>
-  //           <Button colorScheme="guinda" onClick={() => navigate("/students")}>
-  //             Entrar
-  //           </Button>
-  //         </CardFooter>
-  //       </Card>
-  //       <Card align="center" className="flex-1">
-  //         <CardHeader>
-  //           <Heading size="md"> Iniciar sesión como maestro</Heading>
-  //         </CardHeader>
-  //         <CardBody>
-  //           <Text></Text>
-  //         </CardBody>
-  //         <CardFooter>
-  //           <Button colorScheme="blue" onClick={() => navigate("/teacher")}>
-  //             Entrar
-  //           </Button>
-  //         </CardFooter>
-  //       </Card>
-  //     </div>
-  //   </section>
-  // );
 }
--- src/components/LoginButton.jsx
+++ src/components/LoginButton.jsx
@@ -1,3 +1,5 @@
+import { FiArrowRight } from "react-icons/fi";
+
 export function LoginButton({
   student,
   handleMouseEnter,
@@ -11,7 +13,7 @@ export function LoginButton({
         onMouseLeave={handleMouseLeave}
         className={`border-2 h-[480px] w-[400px] relative bg-gradient-to-br ${
           student ? "from-guinda-700" : "from-green-950"
-        } to-gray-600 rounded-2xl p-10 text-white flex flex-col gap-2 transition-all duration-300 transform hover:scale-105 hover:shadow-2xl ${
+        } to-gray-600 rounded-2xl p-10 text-white flex flex-col gap-2 transition-all duration-300 transform hover:scale-105 hover:shadow-2xl  ${
           hover ? "blur-sm grayscale" : ""
         }`}
       >
@@ -24,6 +26,9 @@ export function LoginButton({
           {student ? "Estudiantes" : "Maestros"}
         </h3>
         <h5>Iniciar sesión</h5>
+        <span className="absolute right-8 bottom-8 rounded-full w-14 h-14 flex items-center justify-center backdrop-blur-lg text-3xl border">
+          <FiArrowRight />
+        </span>
       </div>
     </a>
   );
